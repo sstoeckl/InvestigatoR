a55229ab290910deb3cc1c4941f9025706916dee
Eric Schumann <eric.schumanne@uni.li>
2024-04-05 21:07:28 +0000

    Initial commit

---
+++ R/InvestigatoR-package.R
@@ -0,0 +1,31 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+usethis::use_author(
+  given = "Eric",
+  family = "Schumann",
+  role = c("aut", "cre"),
+  email = "eric.schumann@uni.li"
+)
+
+usethis::use_author(
+  given = "Andreas",
+  family = "Pischetsrieder",
+  role = c("aut", "cre"),
+  email = "andreas.pischetsrieder@uni.li"
+)
+
+
+usethis::use_author(
+  given = "Christian",
+  family = "Mahrun",
+  role = c("aut"),
+  email = "christian.mahrun@uni.li"
+)
+
+
+
