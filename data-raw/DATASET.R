f73dffe9d1781e4fe637a5a7ad2f009d88ec68a1
Christian <maruhn.christian@web.dem>
2024-05-18 10:23:25 +0000

    prepare `DATASET`

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,3 @@
 ## code to prepare `DATASET` dataset goes here
-set.seed(8976) # remeber to set your seed!
-my_data <-
-  tibble::tibble(
-    x = runif(100),
-    y = runif(100)
-  )
-usethis::use_data(my_data, overwrite = TRUE)
+load("data-raw/data_ml.RData")
+usethis::use_data(data_ml, overwrite = TRUE)
